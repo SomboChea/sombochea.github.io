27e2da0ea1b3775226a72f7fbbe2a1f012e489a3
Somben Chea <Somben Chea>
2017-05-22 09:48:36 +0700

    Add some feature

--- SETEC/WBDI/js/lang.js
+++ SETEC/WBDI/js/lang.js
@@ -1,47 +1,49 @@
 var langs = [{
-        lang: [
-            { "home": "home" },
-            { "technoloy": "technology" },
-            { "entertainment": "entertainment" },
-            { "sport": "sport" },
-            { "life": "life" },
-            { "aboutus": "aboutus" },
-            { "contactus": "contactus" },
-            { "language": "language" },
-            { "khmer": "khmer" },
-            { "english": "english" },
-            { "signin": "signin" },
-            { "register": "register" },
-            { "videos": "videos" }
-        ]
+        lang: {
+            home: "Home",
+            technology: "Technology",
+            entertainment: "Entertaintment",
+            sport: "Sport",
+            life: "Life &Social",
+            aboutus: "About Us",
+            contactus: "Contact Us",
+            language: "Language",
+            khmer: "Khmer",
+            english: "English",
+            signin: "Sign In",
+            register: "Register",
+            videos: "Videos"
+        }
     },
     {
-        lang: [
-            { "home": "ទំព័រដើម" },
-            { "technoloy": "បច្ចេកវិទ្យា" },
-            { "entertainment": "កម្សាន្ដ" },
-            { "sport": "កីឡា" },
-            { "life": "ជីវិតនិងសង្គម" },
-            { "aboutus": "អំពីយើង" },
-            { "contactus": "ទំនាក់ទំនងយើង" },
-            { "language": "ភាសា" },
-            { "khmer": "ខ្មែរ" },
-            { "english": "អង់គ្លេស" },
-            { "signin": "ចូល" },
-            { "register": "ចុះឈ្មេាះ" },
-            { "videos": "វីដេអូ" }
-        ]
+        lang: {
+            home: "ទំព័រដើម",
+            technology: "បច្ចេកវិទ្យា",
+            entertainment: "កម្សាន្ដ",
+            sport: "កីឡា",
+            life: "ជីវិតនិងសង្គម",
+            aboutus: "អំពីយើង",
+            contactus: "ទំនាក់ទំនងយើង",
+            language: "ភាសា",
+            khmer: "ខ្មែរ",
+            english: "អង់គ្លេស",
+            signin: "ចូល",
+            register: "ចុះឈ្មេាះ",
+            videos: "វីដេអូ"
+        }
+
     }
 ];
 
-function changeLang(id) {
-    if (id == 1) {
-        console.log(langs[0].khmer);
-    } else if (id == 0) {
-        console.log(langs[0].english);
-    } else {
-        console.log("Error!");
-    }
-}
 
-changeLang(0);
+var txtEng = document.getElementById("txtEng");
+var txtKh = document.getElementById("txtKh");
+
+function changeLang(id) {
+
+    var tEng = langs[id].lang.english;
+    var tKh = langs[id].lang.khmer;
+
+    console.log(id + " \n " + tEng + " \n " + tKh);
+
+}
--- SETEC/WBDI/js/main.js
+++ SETEC/WBDI/js/main.js
@@ -8,10 +8,31 @@
 //     alert("Onload function!!");
 // });
 
-$(window).scroll(function () {
+$(window).scroll(function() {
     if ($(this).scrollTop() > 200) {
         $('#navbar-fixed-top').addClass('navbar-fixed-top');
     } else {
         $('#navbar-fixed-top').removeClass('navbar-fixed-top');
     }
 });
+
+
+
+var clickAct = false;
+
+function breakingBtn() {
+
+    if (!clickAct) {
+        var mar = document.getElementById("marquee");
+        var btnActtion = document.getElementById("btnAction");
+        mar.style.opacity = "0";
+        mar.style.visibility = "hidden";
+        clickAct = true;
+    } else {
+        var mar = document.getElementById("marquee");
+        var btnActtion = document.getElementById("btnAction");
+        mar.style.opacity = "1";
+        mar.style.visibility = "visible";
+        clickAct = false;
+    }
+}
